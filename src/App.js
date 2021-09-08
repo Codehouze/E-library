cdb2382def7cf78c1ded781b5145f3aa6e4f8c29
MAC <user@MACs-MacBook-Pro.local>
2021-09-08 03:55:26 -0700

    fixed padding issues

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Image from "react-bootstrap/Image";
 function App() {
   return (
     <Container fluid="lg,sm,sx,md">
-      <Navbar bg="inf" expand="lg">
+      <Navbar className="bg-info" expand="lg">
         <Navbar.Brand href="#home">YBUCK BLOG</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -71,7 +71,7 @@ function App() {
           </center>
         </Col>
       </Row>
-      <Row>
+      <Row className="p-5 m-3">
         <Col>
           <Card style={{ width: "18rem" }}>
             <Card.Img
@@ -129,8 +129,8 @@ function App() {
           fluid
         />
       </Row>
-      <Row className="section">
-        <Col className="section" style={{ bg: "gray" }}>
+      <Row className="section p-5, m-10">
+        <Col className="section1 col-lg-6 col-md-8 col-md-offset-2 col-sm-12 p-5 m-6 m-offset-10" style={{ bg: "gray" }}>
           <h1>What is Lorem Ipsum?</h1>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -144,16 +144,69 @@ function App() {
             PageMaker including versions of Lorem Ipsum.
           </p>
         </Col>
-        <Col className="section2" style={{ bg: "black" }}>
+        <Col className="section2 col-lg-6 col-md-8 col-sm-12" style={{ bg: "black" }}>
           <h1>Why do we use it?</h1>
-<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
-Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
-Various versions have evolved over the years, sometimes by accident, 
-sometimes on purpose (injected humour and the like).</p>
-</Col>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </Col>
       </Row>
-      <Row className="footer">
-        <Col>Footer goes here</Col>
+      <Row className="footer bg-secondary">
+        <Col>
+          <div>
+            Follow us on{" "}
+            <a>
+              <img src="/" />
+              @bookhub
+            </a>
+          </div>
+          <div>paystack paypal Interswitch</div>
+        </Col>
+        <Col>
+          <div>
+            <a href="/">Home</a>
+          </div>
+          <div>
+            <a href="/">Services</a>
+          </div>
+          <div>
+            <a href="/">About Us</a>
+          </div>
+        </Col>
+
+        <Col>
+          <h1>About Us</h1>
+          <div>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source. Lorem Ipsum comes
+              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+              BC. This book is a treatise on the theory of ethics, very popular
+              during the Renaissance. The first line of Lorem Ipsum, "Lorem
+              ipsum dolor sit amet..", comes from a line in section 1.10.32.
+            </p>
+          </div>
+        </Col>
+      </Row>
+      <Row className='bg-info'>
+        <p>
+          <center>Powered By <a href="/">Codehouze</a></center>
+        </p>
       </Row>
     </Container>
     // <div className="App">
